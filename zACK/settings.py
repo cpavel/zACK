c04e070a72ed6597728dede907fc7f5158c2a20c
Futurebound Corp. <pstoev@futurebnd.com>
2025-01-23 14:07:59 -0500

    Add Twitter integration for lead generation
    Add Twitter search functionality to the zACK inbound marketing tool.
    
    * **campaigns/actions.py**
      - Add logic to handle Twitter search results in the `find_leads` function.
      - Implement `generate_twitter_leads` function to fetch and process Twitter leads.
    
    * **campaigns/models.py**
      - Add a new field `twitter_search_terms` to the `Campaign` model to store Twitter search terms.
    
    * **campaigns/serializers.py**
      - Update `CampaignSerializer` to include the new `twitter_search_terms` field.
    
    * **zACK/helpers.py**
      - Implement a new function `search_twitter` to fetch Twitter search results.
      - Add a new class `TwitterClient` to handle Twitter API interactions.
    
    * **zACK/settings.py**
      - Add Twitter API credentials to the settings file.

--- zACK/settings.py
+++ zACK/settings.py
@@ -185,3 +185,8 @@ CELERY_RESULT_SERIALIZER = 'json'
 
 with open('/opt/zACK/system_prompts.json') as f:
     SYSTEM_PROMPTS = json.load(f)
+
+TWITTER_API_KEY = os.getenv('TWITTER_API_KEY', '')
+TWITTER_API_SECRET_KEY = os.getenv('TWITTER_API_SECRET_KEY', '')
+TWITTER_ACCESS_TOKEN = os.getenv('TWITTER_ACCESS_TOKEN', '')
+TWITTER_ACCESS_TOKEN_SECRET = os.getenv('TWITTER_ACCESS_TOKEN_SECRET', '')
